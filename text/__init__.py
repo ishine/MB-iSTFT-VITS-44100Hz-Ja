6a02e2b11776b08041c2e3dabc5fa3f6a6161bc7
AcogiMin <128651897+AcogiMin@users.noreply.github.com>
2023-03-28 18:33:39 +0900

    modify inference

--- text/__init__.py
+++ text/__init__.py
@@ -28,6 +28,18 @@ def text_to_sequence(text, cleaner_names):
   return sequence
 
 
+def infer_g2p(text, cleaner_names):
+  sequence = []
+
+  #clean_text = _clean_text(text, cleaner_names)
+  #print(text)
+  clean_text = pyopenjtalk_g2p_prosody_with_questioin(text)
+  for symbol in clean_text:
+    symbol_id = _symbol_to_id[symbol]
+    sequence += [symbol_id]
+  return sequence
+
+
 def cleaned_text_to_sequence(cleaned_text):
   '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
     Args:
